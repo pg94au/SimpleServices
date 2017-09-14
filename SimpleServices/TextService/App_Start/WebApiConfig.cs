1f6f4e61927b1438006e859728eb1877c35e19f5
Paul Grebenc <paul@grebenc.ca>
2017-09-14 16:02:04 -0400

    Created simple service method for testing.

--- SimpleServices/TextService/App_Start/WebApiConfig.cs
+++ SimpleServices/TextService/App_Start/WebApiConfig.cs
@@ -16,8 +16,8 @@ namespace TextService
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "{controller}",
+                defaults: new { }
             );
         }
     }
---
+++ SimpleServices/TextService/Controllers/TextController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace TextService.Controllers
+{
+    public class TextController : ApiController
+    {
+        [HttpGet]
+        [Route("reverse/{value}")]
+        public HttpResponseMessage Get(string value)
+        {
+            var valueCharArray = value.ToCharArray();
+            Array.Reverse(valueCharArray);
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(new string(valueCharArray));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+
+            return response;
+        }
+    }
+}
